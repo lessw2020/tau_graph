2f18290302d78dfc541de31d3f32779753e53ed3
lessw2020 <lessw@etrillium.com>
2023-01-20 05:36:38 +0000

    testing for inductor rescheduling

---
+++ examples/spmd/inductor_fusion.py
@@ -0,0 +1,172 @@
+import torch
+import torch._dynamo as dynamo
+
+# Returns the result of running `fn()` and the time it took for `fn()` to run,
+# in seconds. We use CUDA events and synchronization for the most accurate
+# measurements.
+def timed(fn):
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    result = fn()
+    end.record()
+    torch.cuda.synchronize()
+    return result, start.elapsed_time(end) / 1000
+
+
+# Generates random input and targets data for the model, where `b` is
+# batch size.
+def generate_data(b):
+    return (
+        torch.randn(b, 3, 128, 128).to(torch.float32).cuda(),
+        torch.randint(1000, (b,)).cuda(),
+    )
+
+
+N_ITERS = 10
+
+from torchvision.models import resnet18
+
+
+def init_model():
+    return resnet18().to(torch.float32).cuda()
+
+
+def eval(mod, inp):
+    return mod(inp)
+
+
+model = init_model()
+# eval_opt = dynamo.optimize("inductor")(eval)
+
+
+inp = generate_data(16)[0]
+# print("eager:", timed(lambda: eval(model, inp))[1])
+# print("dynamo:", timed(lambda: eval_opt(model, inp))[1])
+
+"""eager_times = []
+dynamo_times = []
+for i in range(N_ITERS):
+    inp = generate_data(16)[0]
+    _, eager_time = timed(lambda: eval(model, inp))
+    eager_times.append(eager_time)
+    print(f"eager eval time {i}: {eager_time}")
+
+print("~" * 10)
+
+dynamo_times = []
+for i in range(N_ITERS):
+    inp = generate_data(16)[0]
+    _, dynamo_time = timed(lambda: eval_opt(model, inp))
+    dynamo_times.append(dynamo_time)
+    print(f"dynamo eval time {i}: {dynamo_time}")
+print("~" * 10)
+
+
+
+eager_med = np.median(eager_times)
+dynamo_med = np.median(dynamo_times)
+speedup = eager_med / dynamo_med
+print(
+    f"(eval) eager median: {eager_med}, dynamo median: {dynamo_med}, speedup: {speedup}x"
+)
+print("~" * 10)
+
+"""
+import numpy as np
+
+model = init_model()
+opt = torch.optim.Adam(model.parameters())
+
+
+def train(mod, data):
+    pred = mod(data[0])
+    loss = torch.nn.CrossEntropyLoss()(pred, data[1])
+    loss.backward()
+
+
+eager_times = []
+for i in range(N_ITERS):
+    inp = generate_data(16)
+    opt.zero_grad(True)
+    _, eager_time = timed(lambda: train(model, inp))
+    opt.step()
+    eager_times.append(eager_time)
+    print(f"eager train time {i}: {eager_time}")
+print("~" * 10)
+
+model = init_model()
+opt = torch.optim.Adam(model.parameters())
+train_opt = dynamo.optimize("inductor")(train)
+
+dynamo_times = []
+for i in range(N_ITERS):
+    inp = generate_data(16)
+    opt.zero_grad(True)
+    _, dynamo_time = timed(lambda: train_opt(model, inp))
+    opt.step()
+    dynamo_times.append(dynamo_time)
+    print(f"dynamo train time {i}: {dynamo_time}")
+print("~" * 10)
+
+eager_med = np.median(eager_times)
+dynamo_med = np.median(dynamo_times)
+speedup = eager_med / dynamo_med
+print(
+    f"(train) eager median: {eager_med}, dynamo median: {dynamo_med}, speedup: {speedup}x"
+)
+print("~" * 10)
+
+print(f"custom")
+from typing import List
+
+"""
+def custom_backend(
+    gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]
+):
+    print("custom backend called with FX graph:")
+    gm.graph.print_tabular()
+    return gm.forward
+
+
+# Reset since we are using a different backend (a custom one).
+dynamo.reset()
+opt_model = dynamo.optimize(custom_backend)(init_model())
+opt_model(generate_data(16)[0])
+
+
+def bar(a, b):
+    x = a / (torch.abs(a) + 1)
+    if b.sum() < 0:
+        b = b * -1
+    return x * b
+
+
+opt_bar = dynamo.optimize(custom_backend)(bar)
+inp1 = torch.randn(10)
+inp2 = torch.randn(10)
+opt_bar(inp1, inp2)
+opt_bar(inp1, -inp2)
+
+(
+    explanation,
+    out_guards,
+    graphs,
+    ops_per_graph,
+    break_reasons,
+    explanation_verbose,
+) = dynamo.explain(bar, torch.randn(10), torch.randn(10))
+print(explanation_verbose)
+
+import traceback as tb
+
+try:
+    dynamo.export(bar, torch.randn(10), torch.randn(10))
+except:
+    tb.print_exc()
+
+model_exp = dynamo.export(init_model(), generate_data(16)[0])
+print(f"{model_exp=}")
+print(model_exp[0](generate_data(16)[0]))
+
+"""
---
+++ examples/spmd/inductor_test.py
@@ -0,0 +1,281 @@
+import torch
+from torch import nn
+from torch.fx.experimental.proxy_tensor import make_fx
+from torch._functorch.aot_autograd import (
+    run_functionalized_fw_and_collect_metadata,
+    aot_function,
+)
+import functorch
+from functorch.compile import min_cut_rematerialization_partition
+
+from torch._functorch.eager_transforms import functionalize
+
+from torch import autograd
+from torch._inductor.compile_fx import compile_fx_inner
+
+
+from torch._inductor.decomposition import select_decomp_table
+
+inductor_decomps = select_decomp_table()
+
+
+from copy import copy
+
+all_decomps = copy(inductor_decomps)
+
+from torch._decomp import get_decompositions
+
+all_decomps.update(
+    get_decompositions(
+        [
+            # used in Adam optimizer
+            # TODO: add here https://github.com/pytorch/pytorch/blob/66b324cf06bce720887dabac710aee11b522450f/torch/_inductor/decomposition.py#L20
+            torch.ops.aten.addcdiv.default,
+        ]
+    )
+)
+
+
+def prepare_args(attr_vals, unused_inputs_idx, args):
+    """
+    modify args to pass into a function compiled with make_functional_fx
+    args: original arguments
+    unused_inputs_idx, attr_vals: data returned from make_functional_fx
+    """
+    all_args = list(args) + list(attr_vals)
+    return [a for i, a in enumerate(all_args) if i not in unused_inputs_idx]
+
+
+DEBUG = False
+
+
+def print_graph(name, graph):
+    if DEBUG:
+        print(f"===== {name} ====")
+        graph.print_readable()
+        print(f"===== end {name} ====")
+        graph.graph.print_tabular()
+        print(f"===== end graph print for {name} ====")
+
+
+def print_value(name, value):
+    if DEBUG:
+        print(name, value)
+
+
+def make_functional_fx(fn):
+    """
+    Given a function, does make_fx(functionalize(make_fx(fn))).
+    Some input-related transformations need to be done before functionalize.
+    Returns an GraphModule to be fed to inductor.
+    """
+
+    def call(*args, **kwargs):
+        # 0. hack: run one iteration to initialize optimizer states
+        fn(*args, **kwargs)
+
+        # 1. call make_fx for the first time
+        k1 = make_fx(fn)
+        k2 = k1(*args, **kwargs)
+        print_graph("first_fx", k2)
+
+        # 2. Turn getattr into inputs
+        #    - This is needed for functionalize to work. Functionalize expects
+        #      all tensors to be passed as inputs to the function being functionalized.
+        #    - We collect all concrete getattr inputs into "attr_vals"
+        from collections import defaultdict
+
+        attr_vals_map = dict()
+        attr_nodes = defaultdict(lambda: [])
+
+        for p in k2.graph.nodes:
+            if p.op == "get_attr":
+                val = getattr(k2, p.target)
+                attr_vals_map[p.target] = val
+                attr_nodes[p.target].append(p)
+
+        after_last_placeholder = None
+        for i, p in enumerate(k2.graph.nodes):
+            if p.op != "placeholder":
+                after_last_placeholder = p
+                break
+
+        attr_vals = []
+        with k2.graph.inserting_before(after_last_placeholder):
+            for attr, nodes in attr_nodes.items():
+                placeholder = k2.graph.placeholder(attr)
+                attr_vals.append(attr_vals_map[attr])
+                for node in nodes:
+                    node.replace_all_uses_with(placeholder)
+                    k2.graph.erase_node(node)
+
+        print_value("attr_vals", attr_vals)
+
+        # 3. Remove unused inputs
+        #    - This is needed in order to remove non-tensor inputs before passing this
+        #      graph to inductor. Inductor expects only tensors as inputs
+        #    - This works since all the non-tensor inputs should be unused at this point,
+        #      since in principle we should have converted all getattrs into inputs.
+        #    - We keep the indices of unused inputs in "unused_inputs_idx"
+        unused_inputs = []
+        unused_inputs_idx = []
+
+        for i, p in enumerate(k2.graph.nodes):
+            if p.op != "placeholder":
+                break
+            if len(p.users) == 0:
+                unused_inputs.append(p)
+                unused_inputs_idx.append(i)
+
+        for p in unused_inputs:
+            k2.graph.erase_node(p)
+
+        k2.recompile()
+        print_graph("before_functionalize", k2)
+
+        # 4. call make_fx a second time with functionalization
+        with torch.no_grad():
+            used_args_and_attrs = prepare_args(
+                attr_vals, unused_inputs_idx, args
+            )
+            print(f"141 passed")
+            res = functionalize(k2)  # , decomposition_table=all_decomps
+            print(f"143 passed, {res=}")
+            k3 = make_fx(res, decomposition_table=all_decomps)
+            print(f"143 passed, type k3 = {type(k3)}")
+            print(f"{k3=}")
+            k4 = k3(*used_args_and_attrs, **kwargs)
+        print(f"145 passed")
+        for p in k4.graph.nodes:
+            if p.target == torch.ops.aten.alias.default:
+                p.replace_all_uses_with(p.args[0])
+        print(f"149 passed")
+        print_graph("after functionalize before DCE", k4)
+
+        torch.fx.node._side_effectful_functions.add(
+            torch.ops.aten.copy_.default
+        )
+        print("155 passed")
+        k4.graph.eliminate_dead_code()
+        k4.recompile()
+
+        print_graph("after functionalize after DCE", k4)
+        print(f"159 passed")
+        return k4, attr_vals, unused_inputs_idx
+
+    return call
+
+
+def make_inductor_fn(fn, example_inputs):
+    """
+    Use inductor to compile the function fn.
+    Uses make_functional_fx to trace it passing example_inputs.
+    """
+    fnfx, attr_vals, unused_input_idx = make_functional_fx(fn)(*example_inputs)
+
+    used_args = prepare_args(attr_vals, unused_input_idx, example_inputs)
+    compiled_f = compile_fx_inner(fnfx, used_args)
+
+    def inductor_fn(*args):
+        used_args = prepare_args(attr_vals, unused_input_idx, args)
+        return compiled_f(used_args)
+
+    print(f"+++++++++++++++++++\n")
+    print(f"{inductor_fn=}")
+    return inductor_fn
+
+
+# =====================================
+# 1. Minimal SGD example, no nn.Module
+# =====================================
+
+
+def inplace_test(x, p):
+    loss = torch.sigmoid(torch.matmul(x, p)).sum()
+    # pg, = torch.autograd.grad(loss, p)
+    loss.backward()
+    with torch.no_grad():
+        p -= 0.01 * p.grad
+    return (loss, p)
+
+
+def main_simple():
+    x = torch.randn(10, 2, device="cuda")
+    p = torch.randn(2, 1, requires_grad=True, device="cuda")
+
+    print("before copmile: ", (p * p).sum())
+    compiled_f = make_inductor_fn(inplace_test, [x, p])
+    print("after compile: ", (p * p).sum())
+
+    with torch.no_grad():
+        for _ in range(10):
+            compiled_f(x, p)
+            print("after iteration: ", (p * p).sum())
+
+
+# =====================================
+# 2. Simple example with nn.Module and Optim
+# =====================================
+
+
+class MyModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(4, 2)
+
+    def forward(self, x):
+        return torch.sigmoid(self.linear(x))
+
+
+def train_step(m, o, x, y):
+    o.zero_grad()
+    y_hat = m.forward(x)
+    loss = torch.sum((y_hat - y) ** 2)
+    loss.backward()
+    o.step()
+    return (loss,)
+
+
+def main(optim_cls):
+    x = torch.ones(10, 4, device="cuda")
+    y = torch.ones(10, 2, device="cuda")
+    m = MyModule().to("cuda")
+    sgd = optim_cls(m.parameters(), lr=0.01)
+
+    from copy import deepcopy
+
+    mc = deepcopy(m)
+
+    print("==== Eager ====")
+    # eager
+    for i in range(10):
+        loss = train_step(m, sgd, x, y)
+        print(loss)
+
+    print("==== Compiled ====")
+
+    sgd = optim_cls(mc.parameters(), lr=0.01)
+
+    # warning: even though we'ree passing mc and sgd as "example" inputs,
+    #    the tracer actually takes its tensor states and saves them as part of
+    #    the compiled_f. So mc and sgd cannot be mutated directly afterwards.
+    compiled_f = make_inductor_fn(train_step, [mc, sgd, x, y])
+
+    print(f" Final loop of 10 \n")
+    with torch.no_grad():
+        for i in range(10):
+            # FIXME attention -- mc and sgd are actualy _not_ being used!!
+            #    this is because we collected parameter/tensors from mc and sgd
+            #    during the first tracing, and we're reusing those here instead of
+            #    actually taking them from mc and sgd.
+            (loss,) = compiled_f(mc, sgd, x, y)
+            print(loss)
+
+
+if __name__ == "__main__":
+    main_simple()
+    from functools import partial
+
+    # note we need capturable=True otherwise Adam implementation uses a scalar training step
+    # which doesn't work with make_fx
+    main(partial(torch.optim.Adam, capturable=True, fused=False))
